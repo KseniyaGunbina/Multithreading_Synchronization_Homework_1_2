ccceb4b6177729ab99b576c3dbe566e09e34b0fb
KseniyaGunbina <senyagunbina@yandex.ru>
2022-06-18 19:48:54 +0300

    Удаление лишних коммитов

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,6 @@ public class Main {
         for (int i = 1; i < 4; i++) {
             Thread seller = new Seller(("Покупатель " + i), сarDealership);
             seller.start();
-//            Thread seller2 = new Seller("Покупатель 2", сarDealership);
-//            seller2.start();
-//            Thread seller3 = new Seller("Покупатель 3", сarDealership);
-//            seller3.start();
         }
 
         // поток производителя машин, который ВЫПУСКАЕТ машины на продажу
