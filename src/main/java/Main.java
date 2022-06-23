8701593b75266422962a7aba9938abe2a59b4389
KseniyaGunbina <senyagunbina@yandex.ru>
2022-06-23 21:15:48 +0300

    Промежуточный коммит

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,17 +8,18 @@ public class Main {
         //new Thread(null, сarDealership::productionOfCars, "Производитель машин").start();
 
         // потоки покупателей, которые ПОКУПАЮТ машину (приходят раз в 10 секунд)
-        int wait = 10000;
-        long start = System.currentTimeMillis();
-        int sellerCount = 1;
-        while (sellerCount <= 3) { // создаю трех покупателей
-            if (sellerCount == 1) // убираю задержку при создании первого покупателя
-                new Seller(("Покупатель " + sellerCount++), сarDealership).start();
-            else if ((System.currentTimeMillis() - start) >= wait) // ослальные два создаются с интервалом wait
-                new Seller(("Покупатель " + sellerCount++), сarDealership).start();
-            else
-                continue;
-        }
+//        int wait = 10000;
+//        long start = System.currentTimeMillis();
+//        int sellerCount = 1;
+//        while (sellerCount <= 3) { // создаю трех покупателей
+//            if (sellerCount == 1) // убираю задержку при создании первого покупателя
+//                new Seller(("Покупатель " + sellerCount++), сarDealership).start();
+//            else if ((System.currentTimeMillis() - start) >= wait) // ослальные два создаются с интервалом wait
+//                new Seller(("Покупатель " + sellerCount++), сarDealership).start();
+//            else
+//                continue;
+//        }
+        
 
         // поток производителя машин, который ВЫПУСКАЕТ машины на продажу
         Thread dealer = new Thread(null, сarDealership::productionOfCars, "Производитель машин");
