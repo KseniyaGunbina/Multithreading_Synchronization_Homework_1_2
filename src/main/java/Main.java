46ad91f751013053c5c7ea3fa4cc5517a7122f86
KseniyaGunbina <senyagunbina@yandex.ru>
2022-06-23 21:12:03 +0300

    Промежуточный коммит

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 public class Main {
+    public static final int MAX_COUNT_OF_SALES = 10;
+    public static volatile int count = 0;
     public static void main(String[] args) throws InterruptedException {
         final СarDealership сarDealership = new СarDealership();
 
         // поток производителя машин, который ВЫПУСКАЕТ машины на продажу
-        new Thread(null, сarDealership::productionOfCars, "Производитель машин").start();
+        //new Thread(null, сarDealership::productionOfCars, "Производитель машин").start();
 
         // потоки покупателей, которые ПОКУПАЮТ машину (приходят раз в 10 секунд)
         int wait = 10000;
@@ -17,5 +19,10 @@ public class Main {
             else
                 continue;
         }
+
+        // поток производителя машин, который ВЫПУСКАЕТ машины на продажу
+        Thread dealer = new Thread(null, сarDealership::productionOfCars, "Производитель машин");
+        dealer.setDaemon(true);
+        dealer.start();
     }
 }
