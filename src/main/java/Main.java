fc91b54cfbda6e2445750862bf89651c6b8314be
KseniyaGunbina <senyagunbina@yandex.ru>
2022-06-23 21:05:59 +0300

    Корректировка петодов покупки и выпуска машин

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,18 @@
 public class Main {
+    public static final int MAX_COUNT_OF_SALES = 10;
+    public static volatile int count = 0;
     public static void main(String[] args) throws InterruptedException {
         final СarDealership сarDealership = new СarDealership();
 
         // потоки покупателей, которые ПОКУПАЮТ машину
         for (int i = 1; i < 4; i++) {
-            Thread seller = new Seller(("Покупатель " + i), сarDealership);
-            seller.start();
+            new Seller( "Покупатель " + i, сarDealership).start();
+            //seller.start();
         }
 
         // поток производителя машин, который ВЫПУСКАЕТ машины на продажу
-        new Thread(null, сarDealership::productionOfCars, "Производитель машин").start();
-
-
-
+        Thread dealer = new Thread(null, сarDealership::productionOfCars, "Производитель машин");
+        dealer.setDaemon(true);
+        dealer.start();
     }
 }
--- src/main/java/СarDealership.java
+++ src/main/java/СarDealership.java
@@ -3,59 +3,47 @@ import java.util.List;
 
 public class СarDealership {
     List<Car> cars = new ArrayList<>();
-    private int saleCount;
-    private static final int SELL_TIME = 1000;
-    private static final int PRODUCTION_TIME = 2000;
-    private static final int WAIT_TIME = 5000;
-    private boolean readyForBuy = false;
-    private boolean carDealershipIsOpen = true;
-    private static final int MAX_COUNT_OF_SALES = 10;
+    private static final int SELL_TIME = 500;
+    private static final int PRODUCTION_TIME = 1000;
+    private static final int WAIT_TIME = 2000;
 
-    public synchronized void productionOfCars() {
+    public void productionOfCars() {
         try {
-            while (carDealershipIsOpen) {
-                if (readyForBuy && cars.size() > 0)
-                    wait();
-                else {
-                    System.out.println("====================");
-                    System.out.println("Производитель Ford: Делаю 1 машину.");
-                    Thread.sleep(PRODUCTION_TIME);
-                    cars.add(new Car());
-                    System.out.println("Производитель Ford: 1 машина выпущена в продажу.");
-                    System.out.println("====================");
-                    notifyAll();
+            while (true) {
+                synchronized (this) {
+                    while (cars.size() < 2) {
+                        System.out.println("====================");
+                        System.out.println("Производитель Ford: Делаю 1 машину.");
+                        Thread.sleep(PRODUCTION_TIME);
+                        cars.add(new Car());
+
+                        System.out.println("Производитель Ford: 1 машина выпущена в продажу.");
+                        System.out.println("Количество машин на складе: " + cars.size());
+                        System.out.println("====================\n");
+                        notifyAll();
+                    }
                 }
+                Thread.sleep(WAIT_TIME);
+
             }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
-
-    public synchronized void sellCar(Thread thread) {
+    public synchronized void sellCar() {
         try {
-            while (true) {
-                if (saleCount <= MAX_COUNT_OF_SALES) {
-                    System.out.println(thread.getName() + " пришел в магазин.");
-                    Thread.sleep(WAIT_TIME);
-                    System.out.println(thread.getName() + ": Хочу купить машину!");
-                    readyForBuy = true;
-                    while (cars.size() == 0) {
-                        System.out.println("Машин нет в наличии!");
-                        wait();
-                    }
-                    Thread.sleep(SELL_TIME);
-                    System.out.println("Покупателю " + thread.getName() + " продана 1 машина.");
-                    saleCount++;
-                    readyForBuy = false;
-                    cars.remove(0);
-                    notify();
-                } else
-                    break;
+            System.out.println(Thread.currentThread().getName() + " пришел в магазин.");
+            Thread.sleep(WAIT_TIME);
+            System.out.println(Thread.currentThread().getName() + ": Хочу купить машину!");
+            while (cars.size() == 0) {
+                System.out.println("Машин нет в наличии!");
+                wait();
             }
-            System.out.println("Достигнуто максимальное число продаж, магазин закрывается.");
-            carDealershipIsOpen = false;
-            Thread.interrupted();
-
+            Thread.sleep(SELL_TIME);
+            System.out.println("Покупателю " + Thread.currentThread().getName() + " продана 1 машина.");
+            System.out.println("Количество машин на складе: " + cars.size());
+            cars.remove(0);
+            Main.count++;
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
