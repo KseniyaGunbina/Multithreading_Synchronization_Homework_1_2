0e3e7efeaf38f4f5be154b20ac4a77a4a293112c
KseniyaGunbina <senyagunbina@yandex.ru>
2022-07-06 13:49:02 +0300

    Корректировка критической зоны метода выпуска машин

--- src/main/java/СarDealership.java
+++ src/main/java/СarDealership.java
@@ -18,11 +18,12 @@ public class СarDealership {
                         System.out.println("====================");
                         System.out.println("Производитель Ford: Делаю 1 машину.");
                         Thread.sleep(PRODUCTION_TIME);
-                        cars.add(new Car());
                         System.out.println("Производитель Ford: 1 машина выпущена в продажу.");
+
+                        lock.lock();
+                        cars.add(new Car());
                         System.out.println("Количество машин на складе: " + cars.size());
                         System.out.println("====================\n");
-                        lock.lock();
                         sellerCondition.signal();
                     } catch (InterruptedException ex) {
                         throw new RuntimeException(ex);
